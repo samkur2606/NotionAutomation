f0a52541226a564bf5d1d1f7893dd677b1610763
Kurt, Samil <samil.kurt@ruv.de>
2025-11-20 19:09:48 +0100

    adding hangfire to project

--- NotionAutomation/NotionAutomation.Api/Program.cs
+++ NotionAutomation/NotionAutomation.Api/Program.cs
@@ -1,25 +1,29 @@
+using Hangfire;
+using Hangfire.Storage.SQLite;
+using NotionAutomation.Api.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var hangfireConnection = builder.Configuration.GetConnectionString("Hangfire");
+DirectoryHelper.CreateFolderIfNecessary(hangfireConnection);
+builder.Services.AddHangfire(config => { config.UseSQLiteStorage(hangfireConnection); });
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHangfireServer();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseHangfireDashboard();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
